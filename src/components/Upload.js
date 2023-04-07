0d82e5c1f2f247d1b4d0e3cc88bd8fc3488152f9
harsh7harsh <harshbhatti1989@gmail.com>
2023-04-07 09:59:22 +0530

    err in upload.js

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,42 +1,42 @@
 import React from 'react';
 import smartcontract from './Contract.sol';
 import solc from 'solc';
-// import fs from 'fs';
-
-// function compileSolidityFile(smartcontract) 
-// {
-//   // Read the Solidity file contents
-//     const input = fs.readFileSync(smartcontract, 'utf8');
-
-//   // Compile the Solidity code using solc
-//     const output = solc.compile(input, 1);
-
-//   // Check for any errors during compilation
-//     if (output.errors)
-//     {
-//       throw new Error(`Compilation error: ${output.errors[0]}`);
-//     }
-
-//   // Extract the compiled contract code
-//     const Contract = Object.keys(output.contracts)[0];
-//     const contractBytecode = output.contracts[`${Contract}`].bytecode;
-
-//     return contractBytecode;
-// }
-
-function Fileread(event) {
-  var file = event.target.files[0];
-  var reader = new FileReader();
-  reader.onload = function(event) {
-    // The file's text will be printed here
-    console.log(event.target.result)
-  };
-
-  reader.readAsText(file);
-}
+import fs from 'fs';
 
 function Upload()
 {
+
+  function compileSolidityFile(smartcontract) 
+{
+  // Read the Solidity file contents
+    const input = fs.readFileSync(smartcontract, 'utf8');
+
+  // Compile the Solidity code using solc
+    const output = solc.compile(smartcontract, 1);
+
+  // Check for any errors during compilation
+    if (output.errors)
+    {
+      throw new Error(`Compilation error: ${output.errors[0]}`);
+    }
+
+  // Extract the compiled contract code
+    const Contract = Object.keys(output.contracts)[0];
+    const contractBytecode = output.contracts[`${Contract}`].bytecode;
+
+    return contractBytecode;
+}
+
+  function Fileread(event) {
+    var file = event.target.files[0];
+    var reader = new FileReader();
+    reader.onload = function(event)
+    {
+      // The file's text will be printed here
+      console.log(event.target.result)
+    };
+    reader.readAsText(file);
+  }
   
     return(
         <>
@@ -47,8 +47,7 @@ function Upload()
                    <button className='btn btn-danger' type='submit' value='submit'>Submit</button>
                 </div>
             </div>
-        </form>
-             
+        </form>     
         </>      
     )
 }
